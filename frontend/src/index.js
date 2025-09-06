249ecd7e7cfb56603fa6845439a490fd742e8056
number1-create <100kpublish@gmail.com>
2025-09-06 23:10:23 +0200

    Add files via upload

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'client';
+import ReactDOM from 'react-dom/client'; // <-- Ecco la correzione
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
