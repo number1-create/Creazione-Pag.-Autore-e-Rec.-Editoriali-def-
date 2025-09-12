99014fbdcddbf3d664b0d08e559a915bf8031c7b
number1-create <100kpublish@gmail.com>
2025-09-12 23:45:34 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // <-- Ecco la correzione
+import ReactDOM from 'client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
